537ee7567fcfa095e98539f11efa78137b7d16e5
JulioMendozaC <julio.mendozc@gmail.com>
2023-08-28 22:48:29 -0600

    avances

--- src/components/auth/Login.jsx
+++ src/components/auth/Login.jsx
@@ -1,8 +1,64 @@
+import {useState} from 'react'
+import {Link} from 'react-router-dom'
 
 
 const Login = () => {
+
+    const [user, setUsuario] = useState ({
+        email: '',
+        password: ''
+    });
+    
+    const {email, password} = user ;
+    
+
+    const onChange = (e) =>{
+        setUsuario ({
+            ...user,
+           [e.targe.name] : e.targe.value
+        })
+        
+    }
+
+    const onSubmit = (e) => {
+        e.preventDefault();
+    }
+
     return (  
-        <h1>Desde Login</h1>
+    <div className="form-usuario">
+            <div className="contenedor-form sombra-dark">
+            <h1>Login</h1>
+            <form 
+            onSubmit={onSubmit}>
+                <div className="campo-form">
+                    <label htmlFor="email">Email:</label>
+                        <input 
+                            type="email"
+                            id="email"
+                            name="email"
+                            placeholder="Email"
+                            onChange={onChange} 
+                        />
+                </div>
+                <div className="campo-form">
+                    <label htmlFor="password">Password:</label>
+                        <input 
+                            type="password"
+                            id="password"
+                            name="password"
+                            placeholder="Password"
+                            onChange={onChange}
+                        />
+                </div>
+                <div className="campo-form">
+                    <input type="submit" className="btn btn-primario btn-block" value="Login" />
+                </div>
+            </form>
+            <Link to={'/Register'} className="enlace-cuenta">
+                Sing Up
+            </Link>
+        </div>
+    </div>
     );
 }
  
